ae0dfd167b03acb8d1a43f8ea342f300ca742430
Alejandro <rod24ale@gmail.com>
2025-08-23 12:29:27 -0500

    Servidor LSP y autocompletado

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,31 @@
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "folke/lazydev.nvim",
+  },
+  config = function()
+    -- defaults
+    vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+    vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+    local on_attach = function(_, bufnr)
+      vim.bo[bufnr].omnifunc =  'v:lua.vim.lsp.omnifunc'
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer = bufnr})
+    end
+
+    require("lazydev").setup()
+    -- Si instalo otro lenguaje cambio lua_ls por el nombre del servidor
+    require("lspconfig").lua_ls.setup({
+      on_attach = on_attach,
+      settings = {
+        Lua = {
+          telemetry = { enable = false },
+          workspace = { checkThirdParty = false },
+        }
+      }
+    })
+  end
+}
